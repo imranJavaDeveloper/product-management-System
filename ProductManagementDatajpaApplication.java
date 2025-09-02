13cd83f0748c51f5d61c9b336e3471e2fc551429
Md Imran Alam <151560129+imranJavaDeveloper@users.noreply.github.com>
2025-09-02 23:57:46 +0530

    Add files via upload

---
+++ ProductManagementDatajpaApplication.java
@@ -0,0 +1,11 @@
+package com.cetpa;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementDatajpaApplication.class, args);
+	}
+}
